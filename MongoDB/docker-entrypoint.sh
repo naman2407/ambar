e0f67dcf01e9a9e9a65fbd065722e2146445fd5a
naman2407 <44075457+naman2407@users.noreply.github.com>
2019-02-21 12:00:29 -0500

    Update docker-entrypoint.sh

--- MongoDB/docker-entrypoint.sh
+++ MongoDB/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 echo "storage:" > /etc/mongod.conf
 echo "  wiredTiger:" >> /etc/mongod.conf
 echo "    engineConfig:" >> /etc/mongod.conf
-echo "      cacheSizeGB: ${cacheSizeGB}" >> /etc/mongod.conf
+echo "      cacheSizeGB: 2" >> /etc/mongod.conf
 
 exec mongod --config /etc/mongod.conf
